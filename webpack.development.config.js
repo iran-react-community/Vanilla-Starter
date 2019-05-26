a03d677e48b3135ab85e9921e5879a8d77a6efba
amerllica <amerllica@gmail.com>
2019-05-27 00:11:27 +0430

    Adding show errors on development

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -77,6 +77,7 @@ module.exports = {
       minify: false,
       hash: false,
       cache: false,
+      showErrors: true,
     }),
     new ScriptExtHtmlWebpackPlugin({
       defaultAttribute: 'defer'
